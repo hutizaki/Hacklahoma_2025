695d686834695aef20b42dfcfc5625791a56cae8
hutizaki <hutizaki@gmail.com>
2025-02-15 17:24:33 -0600

    converted to plug-in project

---
+++ PoGC_Eclipse_Plugin/src/testing/test.java
@@ -0,0 +1,16 @@
+package testing;
+
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.jface.window.ApplicationWindow;
+
+
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
